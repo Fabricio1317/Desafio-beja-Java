711a6ce20064b6b019fcf0576f6f0031b2b83980
Fabricio Souza Goncalves <fabricio13.goncalves@gmail.com>
2026-01-29 10:25:05 -0300

    Adicionando o Dockerfile

--- ms-transaction-worker/src/main/java/com/becajava/ms_transaction_worker/infra/dto/TransacaoMockDTO.java
+++ ms-transaction-worker/src/main/java/com/becajava/ms_transaction_worker/infra/dto/TransacaoMockDTO.java
@@ -2,7 +2,6 @@ package com.becajava.ms_transaction_worker.infra.dto;
 
 public record TransacaoMockDTO(
         Long pagadorId,
-        Long recebedorId,
         Double valor,
         String tipo,
         String data
